31a28fa21ace9a6724f1d6efe438a864635ae741
matias685 <78141961+matias685@users.noreply.github.com>
2021-08-26 21:24:04 +0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Install stuff
-sudo apt-get install vim-nox firefox chromium pulseaudio alsa-utils xserver-xorg-core xserver-xorg-video-ati xserver-xorg-input-libinput xkbdata x11-xkb-utils libgl1-mesa-dri xserver-xorg-legacy x11-utils
+sudo apt-get install vim-nox firefox-esr chromium pulseaudio alsa-utils xserver-xorg-core xserver-xorg-video-ati xserver-xorg-input-libinput xkbdata x11-xkb-utils libgl1-mesa-dri xserver-xorg-legacy x11-utils
