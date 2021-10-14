4157777c0c4bdc02210ac2ae6e377623b8e6e198
matias685 <78141961+matias685@users.noreply.github.com>
2021-10-15 00:50:01 +0300

    Delete debian.sh

--- debian.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Install stuff
-sudo apt-get install vim-nox firefox-esr pulseaudio alsa-utils xserver-xorg-core xserver-xorg-video-ati xserver-xorg-input-libinput xkbdata x11-xkb-utils libgl1-mesa-dri xserver-xorg-legacy x11-utils
