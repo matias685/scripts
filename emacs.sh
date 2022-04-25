994e21500ca98d3448433c137d93db890291a3c1
matias685 <78141961+matias685@users.noreply.github.com>
2022-04-25 15:42:09 +0300

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -2,6 +2,6 @@
 git clone git://git.sv.gnu.org/emacs.git
 #sudo apt install build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo
 ./autogen.sh
-./configure --with-native-compilation --with-xwidgets --with-pgtk
+./configure --with-native-compilation --with-pgtk --with-xml2
 make -j6
 sudo make install
