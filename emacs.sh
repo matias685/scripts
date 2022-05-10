da4bc49f273f10b8cb5c2cbfa96873946a7c0c0c
matias685 <78141961+matias685@users.noreply.github.com>
2022-05-11 00:58:20 +0300

    Update emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git clone git://git.sv.gnu.org/emacs.git
+#sudo dnf install autoconf texinfo gtk3-devel libgccjit-devel gnutls-devel ncurses-devel
 #sudo apt install build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo
 ./autogen.sh
 ./configure --with-native-compilation --with-pgtk --with-xml2
