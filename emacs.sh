425a261b0a03fb814769e28b4415fedcd1f34467
matias685 <matias.viitamaki@protonmail.com>
2022-11-30 01:49:29 +0200

    update make configs

--- emacs.sh
+++ emacs.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 git clone git://git.sv.gnu.org/emacs.git
-#sudo dnf install autoconf texinfo gtk3-devel libgccjit-devel gnutls-devel ncurses-devel
-#sudo apt install build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo
+sudo pacman -S base-devel cmake libgccjit
+#sudo dnf install autoconf texinfo gtk3-devel libgccjit-devel gnutls-devel ncurses-devel cmake
+#sudo apt install build-essential libgtk-3-dev libgnutls28-dev libtiff5-dev libgif-dev libjpeg-dev libpng-dev libxpm-dev libncurses-dev texinfo cmake
 ./autogen.sh
-./configure --with-native-compilation --with-pgtk --with-xml2
+./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/lib --localstatedir=/var --mandir=/usr/share/man --without-gconf --without-gpm --with-native-compilation --with-pgtk --with-xml2 'CFLAGS=-march=x86-64 -O2 -pipe'
 make -j6
 sudo make install
