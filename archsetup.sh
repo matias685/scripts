ed1c2f649d94d38bc21b6022555edd8c847b0e2e
matias685 <78141961+matias685@users.noreply.github.com>
2023-06-04 01:05:20 +0300

    Add files via upload

---
+++ archsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo pacman -S --needed fzf lf atool firefox lxappearance-gtk3 udiskie xorg-server pcmanfm-gtk3 dunst xorg-xset xorg-xrandr xwallpaper sxiv picom xorg-xinit redshift lxsession-gtk3 xsettingsd unclutter clipmenu bat xarchiver chromium gnome-themes-extra adwaita-qt5 adwaita-qt6 qt5ct qt6ct alsa-utils flameshot ufw zathura zathura-pdf-mupdf mpd ncmpcpp exa zsh dash man-db rsync reflector
+
+# set dash as default sh
+cd $HOME
+sudo ln -sf dash /bin/sh
+
+# enable firewall
+sudo ufw enable
+sudo systemctl enable --now ufw.service
